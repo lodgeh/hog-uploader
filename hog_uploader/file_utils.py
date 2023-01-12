b8a711c29ea42ea34d585a88a1cb5df2d20bd773
lodgeh <lodgeh@mailbox.org>
2023-01-12 21:35:25 +0000

    add move_file function and missing type hints to other functions

--- hog_uploader/file_utils.py
+++ hog_uploader/file_utils.py
@@ -16,34 +16,39 @@ def get_file_metadata(directory) -> dict:
 
 
 def get_unique_dates(file_dict: dict) -> list:
-    dates = [file_dict[file].date() for file in file_dict]
-    unique_dates = set(dates)
+    dates: list = [file_dict[file].date() for file in file_dict]
+    unique_dates: set = set(dates)
     return [datetime(date.year, date.month, date.day, 0, 0) for date in unique_dates]
 
 
 def groups_files_into_days(file_dict: dict, unique_dates: list) -> list:
-    daily_file_groupings_list = []
+    daily_file_groupings_list: list = []
     for date in unique_dates:
-        start_datetime = date + timedelta(hours=12)
-        end_datetime = date + timedelta(hours=36)
+        start_datetime: datetime = date + timedelta(hours=12)
+        end_datetime: datetime = date + timedelta(hours=36)
 
-        file_list = []
+        file_list: list = []
         for file in file_dict:
-            file_date = file_dict[file]
+            file_date: datetime = file_dict[file]
             if start_datetime <= file_date < end_datetime:
                 file_list.append(file)
 
-        daily_file_group = {}
+        daily_file_group: dict = {}
         daily_file_group[date] = file_list
         daily_file_groupings_list.append(daily_file_group)
     return daily_file_groupings_list
 
 
-def concatenate_videos_and_save_to_output(groups):
+def concatenate_videos_and_save_to_output(groups: list):
     for group in groups:
         for date, files in group.items():
             if not files:
                 continue
-            clips = [VideoFileClip(f"../input/{file}") for file in files]
+            clips: list = [VideoFileClip(f"../input/{file}") for file in files]
             final_clip = concatenate_videoclips(clips)
             final_clip.write_videofile(f"../output/{date.date()}.mp4")
+
+def move_file(file_name: str):
+    output_file_path = f"../output/{file_name}"
+    storage_file_path = f"../storage/{file_name}"
+    os.rename(output_file_path, storage_file_path)
