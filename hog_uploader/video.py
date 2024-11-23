c4e2171d977e6cc302601e8109e64234899a1f84
lodgeh <lodgeh@outlook.com>
2024-11-23 00:03:18 +0000

    started work on video manager class and tests

---
+++ hog_uploader/video.py
@@ -0,0 +1,95 @@
+import os
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+from collections import defaultdict
+
+
+@dataclass
+class Video:
+    file_name: str
+    file_path: str
+    creation_datetime: datetime
+    creation_date_string: str
+
+
+class VideoLoader:
+    def load_videos(self, video_directory_path: str) -> list[Video]:
+        raw_video_list = []
+        video_files = sorted(os.listdir(video_directory_path))
+
+        for file in video_files:
+            if ".MP4" not in file:
+                continue
+
+            name = file.split(".MP4")[0]
+            path = os.path.join(video_directory_path, file)
+
+            file_creation_unix_time = os.path.getmtime(path)
+            file_creation_datetime = datetime.fromtimestamp(file_creation_unix_time)
+
+            raw_video_list.append(
+                Video(
+                    name,
+                    path,
+                    file_creation_datetime,
+                    str(file_creation_datetime.date()),
+                )
+            )
+
+        return raw_video_list
+
+
+class VideoManager:
+    def __init__(self, video_loader: VideoLoader):
+        self.video_loader = video_loader
+        self.raw_video_list = []
+        self.concatenated_video_list = []
+        self.day_grouped_videos = defaultdict(list)
+
+    def get_video_list(self, video_directory_path: str):
+        self.raw_video_list = self.video_loader.load_videos(video_directory_path)
+
+    def group_videos_for_concatenation(self):
+        """
+        we want to group videos into 24 hour periods, starting at midday
+        on the current date and ending at midday for the next date
+
+        e.g. the date 2024-05-25 would include all videos between
+        2024-05-25 12:00:00 and 2024-05-26 11:59:59
+        """
+        for video in self.raw_video_list:
+            period_start = video.creation_datetime.replace(
+                hour=12, minute=0, second=0, microsecond=0
+            )
+
+            if video.creation_datetime >= period_start:
+                group_date = video.creation_datetime.date()
+            else:
+                group_date = video.creation_datetime.date() - timedelta(days=1)
+
+            self.day_grouped_videos[str(group_date)].append(video)
+
+    def concatenate_videos(self):
+        output_path = "output/"
+        for day in self.day_grouped_videos:
+            clips = [
+                VideoFileClip(video.file_path) for video in self.day_grouped_videos[day]
+            ]
+
+            day_clip = concatenate_videoclips(clips)
+            day_clip_file_name = f"{day}.mp4"
+            day_clip_output_path = os.path.join(output_path, day_clip_file_name)
+
+            os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+            day_clip.write_videofile(day_clip_output_path)
+
+            self.concatenated_video_list.append(
+                Video(
+                    day_clip_file_name,
+                    day_clip_output_path,
+                    datetime.now(),
+                    day,
+                )
+            )
---
+++ tests/test_video.py
@@ -0,0 +1,177 @@
+from datetime import datetime
+
+from pytest import MonkeyPatch
+from unittest.mock import MagicMock
+
+from hog_uploader.video import Video, VideoLoader, VideoManager
+from collections import defaultdict
+
+
+class TestVideoLoader:
+    def test_load_videos(self, monkeypatch: MonkeyPatch):
+        # given
+        test_video_loader = VideoLoader()
+        video_directory_path = "test_path"
+
+        monkeypatch.setattr(
+            "os.listdir",
+            lambda video_files: [
+                "DSCF0418.MP4",
+                "DSCF0419.MP4",
+                "DSCF0420.MP4",
+                "DSCF0421.MP4",
+                "some_random_file.text",
+            ],
+        )
+
+        def mocked_getmtime(file_path: str) -> str:
+            video_file_dict = {
+                f"{video_directory_path}/DSCF0418.MP4": 1716761032,
+                f"{video_directory_path}/DSCF0419.MP4": 1716761912,
+                f"{video_directory_path}/DSCF0420.MP4": 1716767472,
+                f"{video_directory_path}/DSCF0421.MP4": 1716767494,
+            }
+            return video_file_dict[file_path]
+
+        monkeypatch.setattr("os.path.getmtime", mocked_getmtime)
+
+        # when
+        actual = test_video_loader.load_videos(video_directory_path)
+
+        # then
+        expected = [
+            Video(
+                "DSCF0418",
+                f"{video_directory_path}/DSCF0418.MP4",
+                datetime(2024, 5, 26, 23, 3, 52),
+                "2024-05-26",
+            ),
+            Video(
+                "DSCF0419",
+                f"{video_directory_path}/DSCF0419.MP4",
+                datetime(2024, 5, 26, 23, 18, 32),
+                "2024-05-26",
+            ),
+            Video(
+                "DSCF0420",
+                f"{video_directory_path}/DSCF0420.MP4",
+                datetime(2024, 5, 27, 0, 51, 12),
+                "2024-05-27",
+            ),
+            Video(
+                "DSCF0421",
+                f"{video_directory_path}/DSCF0421.MP4",
+                datetime(2024, 5, 27, 0, 51, 34),
+                "2024-05-27",
+            ),
+        ]
+        assert expected == actual
+
+
+class TestVideoManager:
+    def test_get_video_list(self):
+        # given
+        video_loader_mock = MagicMock()
+        video_loader_mock.load_videos.return_value = [
+            Video(
+                "test_file_1.MP4",
+                "test_path/test_file_1.MP4",
+                datetime(2024, 11, 22, 0, 0, 0),
+                "2024-11-22",
+            ),
+            Video(
+                "test_file_2.MP4",
+                "test_path/test_file_2.MP4",
+                datetime(2024, 11, 22, 0, 0, 0),
+                "2024-11-22",
+            ),
+        ]
+
+        test_video_mananager = VideoManager(video_loader_mock)
+
+        # when
+        test_video_mananager.get_video_list("test_path")
+
+        # then
+        expected = [
+            Video(
+                "test_file_1.MP4",
+                "test_path/test_file_1.MP4",
+                datetime(2024, 11, 22, 0, 0, 0),
+                "2024-11-22",
+            ),
+            Video(
+                "test_file_2.MP4",
+                "test_path/test_file_2.MP4",
+                datetime(2024, 11, 22, 0, 0, 0),
+                "2024-11-22",
+            ),
+        ]
+
+        assert expected == test_video_mananager.raw_video_list
+
+    def test_group_videos_for_concatination(self):
+        # given
+        test_video_manager = VideoManager(VideoLoader())
+        test_video_manager.raw_video_list = [
+            Video(
+                "test_file_1.MP4",
+                "test_path/test_file_1.MP4",
+                datetime(2024, 5, 26, 12, 0, 0),
+                "2024-05-26",
+            ),
+            Video(
+                "test_file_2.MP4",
+                "test_path/test_file_2.MP4",
+                datetime(2024, 5, 26, 17, 30, 0),
+                "2024-05-26",
+            ),
+            Video(
+                "test_file_3.MP4",
+                "test_path/test_file_3.MP4",
+                datetime(2024, 5, 26, 3, 0, 0),
+                "2024-05-26",
+            ),
+            Video(
+                "test_file_4.MP4",
+                "test_path/test_file_4.MP4",
+                datetime(2024, 5, 25, 23, 0, 0),
+                "2024-05-25",
+            ),
+        ]
+
+        # when
+        test_video_manager.group_videos_for_concatenation()
+
+        # then
+        expected = {
+            "2024-05-26": [
+                Video(
+                    "test_file_1.MP4",
+                    "test_path/test_file_1.MP4",
+                    datetime(2024, 5, 26, 12, 0, 0),
+                    "2024-05-26",
+                ),
+                Video(
+                    "test_file_2.MP4",
+                    "test_path/test_file_2.MP4",
+                    datetime(2024, 5, 26, 17, 30, 0),
+                    "2024-05-26",
+                ),
+            ],
+            "2024-05-25": [
+                Video(
+                    "test_file_3.MP4",
+                    "test_path/test_file_3.MP4",
+                    datetime(2024, 5, 26, 3, 0, 0),
+                    "2024-05-26",
+                ),
+                Video(
+                    "test_file_4.MP4",
+                    "test_path/test_file_4.MP4",
+                    datetime(2024, 5, 25, 23, 0, 0),
+                    "2024-05-25",
+                ),
+            ],
+        }
+        assert expected == test_video_manager.day_grouped_videos
