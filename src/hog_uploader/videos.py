6096800f090d1cf225a4b70a144fe89a6e9588d3
lodgeh <19654724+lodgeh@users.noreply.github.com>
2026-08-23 14:11:31 +0100

    add concatenate videos and move file functions

--- src/hog_uploader/videos.py
+++ src/hog_uploader/videos.py
@@ -1,7 +1,10 @@
+import shutil
 from collections import defaultdict
 from dataclasses import dataclass
 from datetime import date, datetime, timedelta
 from pathlib import Path
+
+from moviepy import VideoFileClip, concatenate_videoclips
 
 
 @dataclass
@@ -9,8 +12,12 @@ class Day:
     date: date
     videos: list[Path]
 
+    @property
+    def date_string(self) -> str:
+        return self.date.isoformat()
 
-def load_videos_2(path: str) -> list[Path]:
+
+def load_videos(path: str) -> list[Path]:
     p = Path(path)
     return [file for file in p.iterdir() if file.suffix == ".mkv"]
 
@@ -19,7 +26,8 @@ def get_days(videos: list[Path]) -> list[Day]:
     days: dict[str, list[Path]] = defaultdict(list)
 
     for video in videos:
-        video_creation_datetime = datetime.fromtimestamp(video.stat().st_mtime)
+        video_creation_timestamp = video.stat().st_mtime
+        video_creation_datetime = datetime.fromtimestamp(video_creation_timestamp)
         day = video_creation_datetime.date()
 
         if video_creation_datetime.hour < 12:
@@ -30,10 +38,33 @@ def get_days(videos: list[Path]) -> list[Day]:
     return [Day(day, videos) for day, videos in days.items()]
 
 
+def concatenate_videos(
+    days: list[Day], archive_directory: Path, output_directory: Path
+) -> None:
+    output_directory.mkdir(parents=True, exist_ok=True)
+    for day in days:
+        videoclips = [VideoFileClip(video) for video in day.videos]
+        final = concatenate_videoclips(videoclips)
+        final.write_videofile(output_directory / f"{day.date_string}.mkv", threads=12)
+
+        for video in day.videos:
+            move_file(video, archive_directory / day.date_string)
+
+
+def move_file(file_path: Path, destination: Path) -> None:
+    destination.mkdir(parents=True, exist_ok=True)
+    shutil.move(file_path, destination)
+
+
 def main():
     path = "/home/hi/hog-uploader/input"
-    videos = load_videos_2(path)
-    get_days(videos)
+    videos = load_videos(path)
+    days = get_days(videos)
+
+    archive_path = Path("archive")
+    output_path = Path("output")
+
+    concatenate_videos(days, archive_path, output_path)
 
 
 if __name__ == "__main__":
--- tests/test_videos.py
+++ tests/test_videos.py
@@ -1,9 +1,19 @@
 from datetime import date, datetime
 from pathlib import Path
 from types import SimpleNamespace
-from unittest.mock import Mock, patch
+from unittest.mock import patch, call
 
-from hog_uploader.videos import Day, get_days
+from hog_uploader.videos import Day, get_days, concatenate_videos, move_file
+
+
+def test_Day():
+    test_date = datetime(2026, 8, 23, 12).date()
+    test_videos = [Path("test.mp4")]
+
+    actual_day = Day(test_date, test_videos)
+
+    assert isinstance(actual_day.date_string, str)
+    assert actual_day.date_string == "2026-08-23"
 
 
 @patch.object(
@@ -13,18 +23,64 @@ from hog_uploader.videos import Day, get_days
         SimpleNamespace(st_mtime=datetime(2026, 8, 22, 12, 1).timestamp()),
         SimpleNamespace(st_mtime=datetime(2026, 8, 22, 12, 5).timestamp()),
         SimpleNamespace(st_mtime=datetime(2026, 8, 22, 11, 1).timestamp()),
+        SimpleNamespace(st_mtime=datetime(2026, 8, 23, 3, 1).timestamp()),
     ],
 )
 def test_get_days(mock_stats, tmp_path):
     video_1 = tmp_path / "video1.mp4"
     video_2 = tmp_path / "video2.mp4"
     video_3 = tmp_path / "video3.mp4"
+    video_4 = tmp_path / "video4.mp4"
 
-    actual = get_days([video_1, video_2, video_3])
+    actual = get_days([video_1, video_2, video_3, video_4])
 
     expected = [
-        Day(date=date(2026, 8, 22), videos=[video_1, video_2]),
+        Day(date=date(2026, 8, 22), videos=[video_1, video_2, video_4]),
         Day(date=date(2026, 8, 21), videos=[video_3]),
     ]
 
     assert actual == expected
+
+
+@patch("hog_uploader.videos.move_file")
+@patch("hog_uploader.videos.concatenate_videoclips")
+@patch("hog_uploader.videos.VideoFileClip")
+def test_concatenate_videos(
+    mock_video_file_clip, mock_concatenate_videoclips, mock_move_files, tmp_path
+):
+    video_1 = tmp_path / "video1.mp4"
+    video_2 = tmp_path / "video2.mp4"
+    video_3 = tmp_path / "video3.mp4"
+
+    test_archive_directory = tmp_path / "archive"
+    test_output_directory = tmp_path / "output"
+
+    test_days = [
+        Day(
+            date=datetime(2026, 8, 23).date(),
+            videos=[video_1, video_2],
+        ),
+        Day(date=datetime(2026, 8, 24).date(), videos=[video_3]),
+    ]
+
+    concatenate_videos(test_days, test_archive_directory, test_output_directory)
+
+    mock_video_file_clip.assert_has_calls([call(video_1), call(video_2), call(video_3)])
+    mock_concatenate_videoclips.call_count == 2
+    mock_move_files.assert_has_calls(
+        [
+            call(video_1, test_archive_directory / "2026-08-23"),
+            call(video_2, test_archive_directory / "2026-08-23"),
+            call(video_3, test_archive_directory / "2026-08-24"),
+        ]
+    )
+
+
+@patch("hog_uploader.videos.shutil.move")
+def test_move_file(mock_move, tmp_path):
+    test_file_path = tmp_path / "some_file.txt"
+    test_destination = tmp_path / "some_destination"
+
+    move_file(test_file_path, test_destination)
+
+    mock_move.assert_called_with(test_file_path, test_destination)
